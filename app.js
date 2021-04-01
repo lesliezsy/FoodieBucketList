a9f301adb3f6815e0d65edf8df75cce89b30f527
Leslie <lesliezsy12@gmail.com>
2021-04-01 22:10:34 +0800

    fix error

--- app.js
+++ app.js
@@ -7,44 +7,57 @@ const restaurantList = require('./restaurant.json')
 
 
 app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
+  defaultLayout: 'main'
 }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('index', { restaurants: restaurantList.results })
+  res.render('index', {
+    restaurants: restaurantList.results
+  })
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
 
-    const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id) 
-    res.render('show', { restaurant })
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', {
+    restaurant
+  })
+
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword;
+
+  // 當搜尋為空字串時候端會有錯誤(Cannot set headers after they are sent to the client)，其主要原因為已經response資料但程式還在執行相關操作。
+
+  // 如果沒輸入關鍵字，就重導回首頁
+  if (keyword === '') {
+    return res.redirect('/');
+  }
+
+  const restaurants = restaurantList.results.filter(restaurant => {
+    // 可以用餐廳的中文或英文名進行搜尋
+    return restaurant.name.toLowerCase().includes(keyword.trim().toLowerCase()) || restaurant.name_en.toLowerCase().includes(keyword.trim().toLowerCase())
 
   })
 
-  app.get('/search', (req, res) => {
-    const keyword = req.query.keyword;
-
-    // 如果沒輸入關鍵字，就重導回首頁
-    if (keyword === '') res.redirect('/');
-    
-    const restaurants = restaurantList.results.filter(restaurant => {
-      // 可以用餐廳的中文或英文名進行搜尋
-      return restaurant.name.toLowerCase().includes(keyword.trim().toLowerCase()) || restaurant.name_en.toLowerCase().includes(keyword.trim().toLowerCase())
-
+  // 如果找不到任何餐廳，就回傳無符合的查詢結果
+  if (restaurants.length === 0) {
+    let noResult = '無符合的查詢結果'
+    return res.render('index', {
+      noResult
     })
-    
-    // 如果找不到任何餐廳，就回傳無符合的查詢結果
-   if (restaurants.length === 0) {
-      let noResult = '無符合的查詢結果'
-      res.render('index', { noResult })
-    }
+  }
 
-    res.render('index', { restaurants, keyword })
-
+  res.render('index', {
+    restaurants,
+    keyword
   })
+
+})
 
 app.listen(port, () => {
-    console.log(`Express is listening on localhost:${port}`)
+  console.log(`Express is listening on localhost:${port}`)
 })
