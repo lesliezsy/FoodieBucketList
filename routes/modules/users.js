a782884fae9893348d43296bacb617efba94e83a
Leslie <lesliezsy12@gmail.com>
2021-05-25 21:36:50 +0800

    feat: add user registration feature

---
+++ routes/modules/users.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../../models/user') // 引入User 
+// const passport = require('passport')
+// const bcrypt = require('bcryptjs')
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+// login時，用 Passport 提供的 authenticate 方法執行認證
+// router.post('/login', passport.authenticate('local', {
+//   successRedirect: '/',
+//   failureRedirect: '/users/login',
+//   failureFlash: true
+// }))
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  // 檢查使用者是否已經註冊
+  User.findOne({ email }).then(user => {
+    console.log("USER: ", user)
+    // 如果已經註冊：退回原本畫面
+    if (user) {
+      console.log('User already exists.')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    } else {
+      // 如果還沒註冊：寫入資料庫
+      return User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    }
+  })
+})
+
+module.exports = router
