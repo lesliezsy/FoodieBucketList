de727616d2ecffda4523440c50de15c15ba2dab3
Leslie <lesliezsy12@gmail.com>
2021-06-01 00:26:25 +0800

    remove redundant console.log

--- config/passport.js
+++ config/passport.js
@@ -32,7 +32,7 @@ module.exports = app => {
     callbackURL: process.env.FACEBOOK_CALLBACK,
     profileFields: ['email', 'displayName']
   }, (accessToken, refreshToken, profile, done) => {
-    console.log(profile)
+    
     const { name, email } = profile._json
     User.findOne({ email })
       .then(user => {
